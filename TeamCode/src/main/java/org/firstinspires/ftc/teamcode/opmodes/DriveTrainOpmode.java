bb82518ec93de1a078eca9dc129aa594e6bbd47e
aiden <aiden.lee.sd@gmail.com>
2024-07-15 21:20:25 -0700

    Opmode committed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveTrainOpmode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Opmode")
+public class DriveTrainOpmode extends LinearOpMode{
+    @Override
+    public void runOpMode() {
+        DcMotor myMotor = hardwareMap.get(DcMotor.class, "myMotor");
+        waitForStart();
+        double x = 0;
+        while (opModeIsActive()){
+            myMotor.setPower(Math.sin(x));
+            x += 0.00000001;
+        }
+    }
+}
