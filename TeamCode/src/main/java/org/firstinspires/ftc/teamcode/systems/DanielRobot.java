a3b6d81af7baf7eded08f8966e38b0138250a4d5
df4edu-ftc <df4edu+ftc@gmail.com>
2024-08-08 11:41:10 +0300

    Implemented DanielSlides.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DanielRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DanielRobot.java
@@ -7,14 +7,17 @@ import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 public class DanielRobot {
 	public HardwareQueue hardwareQueue;
 	public DanielIntake intake;
+	public DanielSlides slides;
 
 	public DanielRobot(HardwareMap hardwareMap) {
 		hardwareQueue = new HardwareQueue();
 		intake = new DanielIntake(hardwareMap, hardwareQueue);
+		slides = new DanielSlides(hardwareMap, hardwareQueue);
 	}
 
 	public void update(){
 		intake.update();
+		slides.update();
 		hardwareQueue.update();
 	}
 }
