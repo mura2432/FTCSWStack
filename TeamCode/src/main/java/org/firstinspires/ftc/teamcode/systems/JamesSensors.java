e4e2bcef87a387b5e1a73ba13190deac888747dd
mura2432 <111770840+mura2432@users.noreply.github.com>
2024-08-10 14:56:18 -0700

    Fixed Sensors Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/JamesSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/JamesSensors.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.systems;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
 
 public class JamesSensors {
+    private LynxModule controlHub, expansionHub;
     private int slidesEncoder;
     private final HardwareMap hardwareMap;
     private final HardwareQueue hardwareQueue;
@@ -15,9 +17,22 @@ public class JamesSensors {
         this.hardwareMap = hardwareMap;
         this.hardwareQueue = hardwareQueue;
         this.robot = robot;
+
+        initSensors(hardwareMap);
     }
 
+    private void initSensors(HardwareMap hardwareMap){
+        controlHub = hardwareMap.get(LynxModule.class, "Control Hub");
+        controlHub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+
+        expansionHub = hardwareMap.get(LynxModule.class, "Expansion Hub");
+        expansionHub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+    }
     public void update(){
+        updateControlHub();
+    }
+
+    private void updateControlHub(){
         slidesEncoder = ((PriorityMotor) hardwareQueue.getDevice("rightFront")).motor[0].getCurrentPosition() * -1;
     }
 
