9c5d5678b2ff16855cd9cb27eea0cfbe29d87afa
Aiden Lee <aiden.lee.sd@gmail.com>
2024-08-15 22:12:22 -0700

    Error Message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AidenRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AidenRobot.java
@@ -2,19 +2,25 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.AidenDriveTrain;
+import org.firstinspires.ftc.teamcode.systems.AidenSensors;
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 
 public class AidenRobot{
-    public HardwareQueue hardwareQueue;
-    public HardwareMap hardwareMap;
+    public HardwareMap hardwareMap = new HardwareMap();
+    public AidenSensors sensors;
+    public AidenDriveTrain drivetrain;
     public AidenIntake intake;
     public AidenSlides slides;
 
-    public AidenRobot(){
-        hardwareQueue = new HardwareQueue();
-        hardwareMap = new HardwareMap()
-        intake = new AidenIntake(hardwareQueue, hardwareMap);
-        slides = new AidenSlides(hardwareQueue, hardwareMap);
+    public HardwareQueue hardwareQueue = new HardwareQueue();\
+
+    public AidenRobot(HardwareMap hardwareMap) {
+       this.hardwareMap = hardwareMap;
+        sensors = new AidenSensors();
+        drivetrain = new AidenDriveTrain();
+        intake = new AidenIntake();
+        slides - new AidenSlides();
     }
     public void update(){
         hardwareQueue.update();
